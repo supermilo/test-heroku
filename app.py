cd959e897d61619fcce891d1309451ad4aaab8bb
supermilo <supermilo_1@hotmail.com>
2021-02-24 21:44:13 -0600

    third try

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ api = Api(app)
 db.init_app(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
